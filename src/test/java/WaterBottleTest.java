cd9bb0101a83b2fe1a45bde4372c0bd36d442efd
danyibalazs <danyi.balage@gmail.com>
2020-03-23 16:39:33 +0000

    water bottle added, tested

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        this.waterBottle = new WaterBottle();
+    }
+
+    @Test
+    public void testGetVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void testDrink(){
+        waterBottle.drink();
+        assertEquals(90, waterBottle.getVolume());
+    }
+
+    @Test
+    public void testEmpty(){
+        waterBottle.empty();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void testFill(){
+        waterBottle.fill();
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+}

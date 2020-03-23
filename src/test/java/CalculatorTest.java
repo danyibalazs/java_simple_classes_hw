d18193828e3c770a92551023d4a5ce8001164bf2
danyibalazs <danyi.balage@gmail.com>
2020-03-23 16:00:29 +0000

    calculator added, tested

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
 
-    public int add(int num1, int num2){
+    Calculator calculator;
 
+    @Before
+    public void before(){
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(8, calculator.add(3,5));
+    }
+
+    @Test
+    public void testSubtract(){
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiply(){
+        assertEquals(12, calculator.multiply(3, 4));
+    }
+
+    @Test
+    public void testDivide(){
+        assertEquals(3.5, calculator.divide(7, 2), 0.01);
     }
 }
